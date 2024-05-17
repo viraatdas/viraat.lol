eef4539999a0332518aa1f9619301c6a574d5e47
Viraat Das <viraat.laldas@gmail.com>
2024-05-16 21:21:07 -0700

    Revert "added mobile view"
    This reverts commit 1557584664e64eb91276bed54a7ec57c05f81069.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,52 +4,45 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
-  header: [
-    Component.MobileOnly(
-      Component.ExplorerBurger({
-        folderDefaultState: "open",
-        folderClickBehavior: "link",
-      }),
-    ),
-    Component.MobileOnly(Component.Spacer()),
-    Component.MobileOnly(Component.PageTitle()),
-    Component.Search(),
-    Component.Darkmode(),
-  ],
+  header: [],
   footer: Component.Footer({
     links: {
-      Github: "https://github.com/viraatdas/viraat.lol/",
+      GitHub: "https://github.com/viraatdas/viraat.lol",
     },
   }),
 }
 
+// components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
-    Component.ContentMeta({ showReadingTime: false }),
+    Component.ContentMeta(),
     Component.TagList(),
   ],
   left: [
-    Component.DesktopOnly(Component.PageTitle()),
-    Component.DesktopOnly(
-      Component.ExplorerBurger({
-        folderClickBehavior: "link",
-        folderDefaultState: "collapsed",
-        useSavedState: true,
-        title: "",
-      }),
-    ),
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.DesktopOnly(Component.Graph()),
-    Component.TableOfContents(),
-    Component.DesktopOnly(Component.Backlinks()),
+    Component.Graph(),
+    Component.DesktopOnly(Component.TableOfContents()),
+    Component.Backlinks(),
   ],
 }
 
+// components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: defaultContentPageLayout.beforeBody,
-  left: defaultContentPageLayout.left,
+  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
+  left: [
+    Component.PageTitle(),
+    Component.MobileOnly(Component.Spacer()),
+    Component.Search(),
+    Component.Darkmode(),
+    Component.DesktopOnly(Component.Explorer()),
+  ],
   right: [],
 }
