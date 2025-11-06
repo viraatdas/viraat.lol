8ef74d0e310024991326bc191710f48e000a25f8
Viraat Das <viraat.laldas@gmail.com>
2025-11-05 17:51:13 -0800

    Add 'Made by Viraat' link to footer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -8,6 +8,7 @@ export const sharedPageComponents: SharedLayout = {
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/viraatdas/notes",
+      "Made by Viraat": "https://viraat.dev/",
     },
   }),
 }
